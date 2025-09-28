a7bff475ee9bc5a8030b354947369d766fd23b2a
urmom6009 <158238628+urmom6009@users.noreply.github.com>
2025-09-28 10:16:47 -0700

    turning into PWA for rapid iOS experience and dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+const config: NextConfig = { };
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default withPWA(config);
